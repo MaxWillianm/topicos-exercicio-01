3331d8a00d2aea09e84412fa7aeff7ac925f04de
Max Willian <87044552+MaxWillianm@users.noreply.github.com>
2022-03-23 19:12:13 -0300

    exercicio feito

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const Titulo = function(){
+  return (
+    <h1>Lista de Compras</h1>
+  )}
+
+  const Lista = function({itemList}){
+    return (
+      <ul>
+        {itemList.map((item) => (
+          <li><strong>{item}</strong></li>
+        ))}
+      </ul>
+    )
+  }
+
 function App() {
+
+  const [item, setItem] = useState('');
+  const [itemList, setItemList] = useState([])
+  const handleRemoveItem = React.useCallback((todo) => {
+    let newItemList = [itemList];
+    newItemList.splice(itemList.indexOf(todo), 1)
+    setItemList(newItemList);
+  }, [itemList]);
+  
+  const addItem = () => {
+    setItemList([item].concat(itemList))
+    setItem('')
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Titulo></Titulo>
+      <br/>
+      <br/>
+      <input type="text" placeholder="Item" value={item} name="item" onChange = {e => setItem(e.target.value)} />
+      <button class="btn btn-primary" onClick={addItem}>Adicionar Item</button>
+      <button class="btn btn-primary" onClick={e => handleRemoveItem(handleRemoveItem)}> Limpar Lista </button>
+      
+      <Lista itemList={itemList}></Lista>
+    
+    </div> 
   );
 }
+
 
 export default App;
